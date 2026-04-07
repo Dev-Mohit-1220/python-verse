058f5754de89387c25168c5e72cf684b155a40b8
Dev-Mohit-1220 <dev.mike.1220@gmail.com>
2026-04-07 12:49:13 +0530

    Add scenario of functions

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 def main() -> None:
     print("Hello python", "From Mohit Jogiwala")
 
+def greet(name: str = "Guest") -> None:
+    print("Hello", name)
+
+def add(a: int, b: int) -> int:
+    return a+b
+
+def sum_all(*nums: int) -> int:
+    total: int = 0
+    for n in nums:
+        total += n
+    return total
+
 if __name__ == "__main__":
     main()
+    greet("Mohit")
+    print("Addition", add(2,3))
+    print("Sum of all numbers", sum_all(1,2,3,4))
